a2b1825c341fd249e7b8b3dddd1bdd9e29ca3ed1
Lucas Monteiro Ribeiro <luckas.ribeiro2015@gmail.com>
2018-11-22 08:52:06 -0200

    arrumar animação

--- animacoes.js
+++ animacoes.js
@@ -1,3 +1,19 @@
+console.log("Achei")
+
+
 function mostrar (){
-	var teste = document.getElementById("teste1").style.display: "block" 
+	var teste = document.getElementById("esconder");
+	if(teste.style.visibility == "hidden"){
+		teste.style.visibility = "visible"
+	}else{
+		teste.style.visibility = "hidden";	
+	}
+
+
+
+	// console.log("teste : ",teste.style.display );
+	
+
+
+	// if (id ==  ) 
 }
