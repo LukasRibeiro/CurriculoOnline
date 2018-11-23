aa714b4678bba0ccf9fc52888e3f7fc6eecd8b54
Lucas Monteiro Ribeiro <luckas.ribeiro2015@gmail.com>
2018-11-23 08:58:57 -0200

    finalizado | rever o codigo e readme

--- animacoes.js
+++ animacoes.js
@@ -1,17 +1,22 @@
-function show (){
-	var teste = document.getElementById("esconder");
-	
-	if(teste.style.visibility == "visible"){
-		teste.style.visibility = "hidden";
-	}
-	teste.style.opacity = 0;
-
-	// else{
-	// 	teste.style.visibility = "visible";
-	// }
-	// teste.style.opacity = 1;
+function welcome (){
+	alert("Bem vindo");
+	alert("Clique no icone curriculo acima para mais informações")
 }
 
+
+
+ function show (){
+	var show = document.getElementById("esconder");
+	
+	if(show.style.visibility == "visible"){
+		show.style.visibility = "hidden";
+		show.style.opacity = 0;
+	}
+	else{
+		show.style.visibility = "visible";
+		show.style.opacity = 1;
+	}
+}
 
 // teste.style.opacity = 1;
 
